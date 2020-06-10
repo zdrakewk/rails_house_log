396ad7a868f23f777c369d93c50dfb09a78b2524
github username <github email address>
2020-06-09 19:15:15 -0500

    Controller g Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/users/new'
+  # get 'users/create'
+  post '/users', to: 'users#create', as: 'create_user'
+  get '/users/:id', to: 'users#show'
+  
   resources :houses #,only: || except: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
